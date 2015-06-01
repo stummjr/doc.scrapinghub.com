f6096f673d71bea2ef267f7f0fb964f43bd1398e
Richard Dowinton <richard.dowinton@gmail.com>
2015-06-01 16:02:18 +0100

    Updated import statements for Crunchbase spider example

--- _static/sc-crunchbase.py
+++ _static/sc-crunchbase.py
@@ -1,15 +1,12 @@
-from scrapy.spider import Spider
-from scrapy.selector import Selector
-from scrapy.http import Request
-from scrapy.item import Item, Field
+import scrapy
 from scrapy.contrib.loader import ItemLoader
 from scrapy.contrib.loader.processor import MapCompose, TakeFirst
 from urlparse import urljoin
 
-class Company(Item):
-    name = Field()
-    logo = Field()
-    website = Field()
+class Company(scrapy.Item):
+    name = scrapy.Field()
+    logo = scrapy.Field()
+    website = scrapy.Field()
 
 
 class CompanyLoader(ItemLoader):
@@ -17,15 +14,14 @@ class CompanyLoader(ItemLoader):
     default_output_processor = TakeFirst()
 
 
-class CrunchbaseSpider(Spider):
+class CrunchbaseSpider(scrapy.Spider):
     name = "crunchbase"
     allowed_domains = ["crunchbase.com"]
     start_urls = ['http://www.crunchbase.com/companies?q=pets']
 
     def parse(self, response):
-        s = Selector(response)
-        for x in s.xpath("//table[@class='col2_table_listing']//a/@href").extract():
-            yield Request(urljoin(response.url, x), self.parse_company)
+        for x in response.xpath("//table[@class='col2_table_listing']//a/@href").extract():
+            yield scrapy.Request(urljoin(response.url, x), self.parse_company)
 
     def parse_company(self, response):
         l = CompanyLoader(item=Company(), response=response)
