45aef81ea704874b4abdfeab5788cbcb68c772de
Bernard <Bernard@scrapinghub.com>
2015-08-13 14:21:53 +0200

    wrapped urls in encodeURI()

--- _static/crawlera-casperjs.js
+++ _static/crawlera-casperjs.js
@@ -6,12 +6,12 @@ var casper = require('casper').create();
 // 1. session authentication
 casper.start();
 casper.setHttpAuth('<API key>', '');  // set up authentication for entire session
-casper.start('http://paygo.crawlera.com:8010/fetch?url=http://wtfismyip.com/text', function(response) {
+casper.start(encodeURI('http://paygo.crawlera.com:8010/fetch?url=http://wtfismyip.com/text'), function(response) {
     this.echo(response.status);  // print response status
 });
 // or
 //  2. call url through authentication and crawlera directly for each url
-casper.start('http://<API key>:@paygo.crawlera.com:8010/fetch?url=http://wtfismyip.com/text', function(response) {
+casper.start(encodeURI('http://<API key>:@paygo.crawlera.com:8010/fetch?url=http://wtfismyip.com/text'), function(response) {
     this.echo(response.status);  // print response status
 });
 
